f7df21ada4debf99d4ab43bcf2f1a2726745c381
Rachel Coulter <rachel@coulter.ninja>
2017-11-02 13:47:08 -0400

    Now I can edit my articles!

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,19 +8,36 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def new
+    @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
 
-    @article.save
+   if @article.save
     redirect_to @article
+   else
+     render 'new'
+     end
   end
 
-    private
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+   private
     def article_params
       params.require(:article).permit(:title, :text)
     end
 end
-
