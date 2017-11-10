b58a1840524662db04b11cffae7a0c4802fa56a7
Rachel Coulter <rachel@coulter.ninja>
2017-11-10 13:59:48 -0500

    My destroy action isn't working but i should save anyway.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
-class ArticlesController < ApplicationController
 
+class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
@@ -8,22 +8,22 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
-
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
     @article = Article.new(article_params)
 
-   if @article.save
-    redirect_to @article
-   else
-     render 'new'
-     end
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   def update
@@ -36,8 +36,15 @@ class ArticlesController < ApplicationController
     end
   end
 
-   private
-    def article_params
-      params.require(:article).permit(:title, :text)
-    end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
